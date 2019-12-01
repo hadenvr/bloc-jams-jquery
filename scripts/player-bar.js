0eca944af388f3bba3ab4a08209f281779810e84
Sean <brian.sean.kim@gmail.com>
2019-12-01 14:14:53 -0500

    Fixed Previous Button Logic

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -19,6 +19,21 @@
     player.playPause(nextSong);
   });
 
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') {
+      return;
+    }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0) {
+      return;
+    }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
+
   $('#time-control input').on('input', function(event) {
     player.skipTo(event.target.value);
   });
@@ -34,18 +49,4 @@
     $('#time-control input').val(percent);
   }, 1000);
 
-  $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') {
-      return;
-    }
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex >= album.songs.length) {
-      return;
-    }
-
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
-  });
 }
