3d839393f9e9603a5caca730ca70b3aa617317dd
Sean <brian.sean.kim@gmail.com>
2019-12-01 00:00:22 -0500

    Player Bar Button Assignment Submission

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,17 +1,36 @@
 {
   $('button#play-pause').on('click', function() {
-     player.playPause();
-     $(this).attr('playState', player.playState);
-   });
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
 
-   $('button#next').on('click', function() {
-     if (player.playState !== 'playing') { return; }
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') {
+      return;
+    }
 
-     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex + 1;
-     if (nextSongIndex >= album.songs.length) { return; }
-     
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
-   });
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const nextSongIndex = currentSongIndex + 1;
+    if (nextSongIndex >= album.songs.length) {
+      return;
+    }
+
+    const nextSong = album.songs[nextSongIndex];
+    player.playPause(nextSong);
+  });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') {
+      return;
+    }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex >= album.songs.length) {
+      return;
+    }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
 }
